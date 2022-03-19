e4ebac11e6f26b8ee00ec1a27fc68cf4af089851
Jangmanbo <wkddpdnjs99@gmail.com>
2022-03-20 04:05:58 +0900

    UE4 C++ 프로그래밍 입문 (2) - 변수와 UPROPERTY | 언리얼 4

---
+++ UnrealVariables/Source/UnrealVariables/UnrealVariablesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealVariablesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALVARIABLES_API AUnrealVariablesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
