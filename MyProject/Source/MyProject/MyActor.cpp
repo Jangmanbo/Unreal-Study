f3b688e8759fa29946058c1ba8d14e33c7823a16
Jangmanbo <wkddpdnjs99@gmail.com>
2022-03-28 07:33:36 +0900

    로그 출력하기 | 언리얼4

--- MyProject/Source/MyProject/MyActor.cpp
+++ MyProject/Source/MyProject/MyActor.cpp
@@ -2,6 +2,7 @@
 
 
 #include "MyActor.h"
+#include "MyProject.h"
 
 // Sets default values
 AMyActor::AMyActor()
@@ -15,8 +16,30 @@ AMyActor::AMyActor()
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Log, TEXT("BeginPlay"));
+	UE_LOG(LogTemp, Error, TEXT("Error Message"));
+	UE_LOG(LogTemp, Warning, TEXT("Warning Message"));
+	UE_LOG(LogTemp, Display, TEXT("Display Message"));
 
+	// 직접 생성한 로그 카테고리 (프로젝트이름.h, cpp에 선언)
+	UE_LOG(MyLogCategory, Log, TEXT("My Category Log"));
+
+	FString CharacterName = TEXT("HiWer");
+	UE_LOG(LogTemp, Log, TEXT("Character Name = %s"), *CharacterName);
+
+	bool isAttackable = true;
+	UE_LOG(LogTemp, Log, TEXT("Is Attackable = %s"), isAttackable ? TEXT("true") : TEXT("false"));
+
+	int hp = 100;
+	UE_LOG(LogTemp, Log, TEXT("HP = %d"), hp);
+
+	float AttackSpeed = 1.0f;
+	UE_LOG(LogTemp, Log, TEXT("Attack Speed = %f"), AttackSpeed);
+
+	FVector CharacterPosition = GetActorLocation();
+	UE_LOG(LogTemp, Log, TEXT("Position = %s"), *CharacterPosition.ToString());
+
+	// 여러 타입의 변수를 한 번에 로그 출력
+	UE_LOG(LogTemp, Log, TEXT("Character Name = %s, HP = %d, Attack Speed = %f"), *CharacterName, hp, AttackSpeed);
 }
 
 // Called every frame
